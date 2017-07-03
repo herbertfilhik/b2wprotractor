643cd8fc54a45f25a4a125c924a075a0b94d2f5e
default <default@Lindao.local>
2017-07-03 18:22:35 -0300

    executa com 2 intancias chrome e firefox

--- protractorConf.js
+++ protractorConf.js
@@ -1,27 +1,10 @@
-// An example configuration file.
 exports.config = {
-  directConnect: true,
-
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  onPrepare: function() {
-    var width = 1600;
-    var height = 1200;
-    browser.driver.manage().window().setSize(width, height);
-  },
-
-  // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
-
-  // Spec patterns are relative to the current working directory when
-  // protractor is called.
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['HomePageSpec.js'],
-
-  // Options to be passed to Jasmine.
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
-};
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+    browserName: 'chrome'
+  }]
+}
